e46677a57beffb033983886bd0296ff92ad232dc
Alex Brown <alexanderbrown.info@gmail.com>
2017-10-11 09:13:18 +0100

    added presentational header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,11 @@
-import React, {Component} from 'react'
+import React from 'react';
 
-export default function() {
-  return <h1>Hello World!</h1>
+import Header from './header';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
