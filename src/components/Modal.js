50653e9d0e851eaa283155ce06bd7b4bfab15774
Alex Brown <alexanderbrown.info@gmail.com>
2017-10-11 11:21:43 +0100

    added basic modal functionality

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 const Modal = props => {
-  console.log(props);
-  return (
-    <div className="mask">
-      <div className="modal-panel">
-        <h1>Modal!</h1>
+  const {winner, draw, running, startNewGame} = props;
+    return (
+      <div>
+        {!running &&
+          <div className="mask">
+            <div className="modal-panel">
+              {winner && <p>{`Winner ${winner}`}</p>}
+              {draw && <p>It's a draw!</p>}
+              <p>'Start a new game...'</p>
+              <button onClick={startNewGame}>New Game</button>
+            </div>
+          </div>
+        }
       </div>
-    </div>
-  );
+    );
+
 };
 
 export default Modal;
