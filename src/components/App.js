eb6755eb6a2e551a24cb55b9e80e1413d61e00cb
Alex Brown <alexanderbrown.info@gmail.com>
2017-10-11 09:22:55 +0100

    completed header component and plugged App into reducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
+import game from '../reducers/gameReducer';
 import Header from './header';
+
+let store = createStore(game);
 
 export default function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <Provider store={store}>
+      <div>
+        <Header />
+      </div>
+    </Provider>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Tic-Tac-Toe</h1>
+      <p>by Alexander Brown</p>
+    </div>
+  );
+}
