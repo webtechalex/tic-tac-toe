1f55b8312a1b37e3790dd2748a2a77a3b5ead1c8
Alex Brown <alexanderbrown.info@gmail.com>
2017-10-11 13:18:53 +0100

    deleted unneccessary Cell component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Cell from './Cell';
 import detectWin from '../helpers';
 
 class Board extends React.Component {
--- src/components/Cell.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Cell = (props) => {
-  return (
-    <div className="cell" onClick={props.handleCellClick}>{props.value}</div>
-  );
-}
-
-export default Cell;
